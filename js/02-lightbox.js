9eed799e24e3b3fd17115188eee10078fe3af62b
Yuraro <romanykyriy@gmail.com>
2023-03-28 19:59:56 -0700

    Update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,35 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 
-const galleryListEllements = document.querySelector('.gallery');
+    const galleryListEllements = document.querySelector('.gallery');
 
-galleryListEllements.innerHTML = makeGallery(galleryItems);
-galleryListEllements.addEventListener("click", onImgClick);
+    galleryListEllements.innerHTML = makeGallery(galleryItems);
+    galleryListEllements.addEventListener("click", onImgClick);
 
-function makeGallery(params) {
-    return params
+    function makeGallery(params) {
+        return params
         .map((param) => {
             return `<li class="gallery__item">
-    <a class="gallery__link" href="${param.original}">
-    <img
-        class="gallery__image"
-        src="${param.preview}"
-        alt="${param.preview}"
-    />
-    </a>
-    </li>`;
+            <a class="gallery__link" href="${param.original}">
+                <img class="gallery__image" src="${param.preview}" alt="${param.description}">
+            </a>
+            </li>`;
         })
         .join("");
-}
-function onImgClick(event) {
-    if (event.target.className !== "gallery__image") {
-        return;
     }
-    event.preventDefault();
-    const modal = new SimpleLightbox(`.gallery .gallery__link`, {
-        captionDate: "alt",
+
+    function onImgClick(event) {
+        if (event.target.className !== "gallery__image") {
+        return;
+        }
+        event.preventDefault();
+        const modal = new SimpleLightbox(`.gallery .gallery__link`, {
+        caption: true,
+        captionsData: "alt",
         captionDelay: 250,
-    });
-    console.log(modal);
-    modal.open();
-}
-console.log(galleryItems);
+        });
+        modal.open();
+    }
+
+    console.log(galleryItems);
